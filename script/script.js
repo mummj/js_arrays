264a79b772c94bd39ee2a896413d9d1adedc36ee
Jason Mumm <jasonmumm@Jasons-MacBook-Pro.local>
2021-02-19 09:18:25 -0800

    day 1 of arrays

---
+++ script/script.js
@@ -0,0 +1,20 @@
+console.log('hello');
+
+let items = ['pencil','hat', 'book' ];
+
+console.log(items[0]);
+console.log(items[2]);
+
+console.log(items.length);
+
+items.push('laptop');
+
+console.log('new length', items.lenghth);
+
+console.log('new item is', items[3]);
+
+console.log(items.pop());
+
+items.push('suitcase');
+
+console.log(items);
